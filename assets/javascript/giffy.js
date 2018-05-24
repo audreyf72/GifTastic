c96f0842649931b791423064249f0cc7de7a253d
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-05-23 17:10:27 -0700

    second commit

--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,20 +1,39 @@
 $(document).ready(function() {
 
-var button = $("<input/>").attr({type: "button", name: "giffyButtom", value:"Dogs"});
-    $(".buttons").append(button);
+//renders buttons
+function renderButtons() {
+  $('#buttons').empty();
+
+  for (var i = 0; i < giffyGifs.length; i++) {
+
+      var newButtons = $("<button class='btn btn-primary btn-lg active'>");
+      newButtons.attr("data-gif", giffyGifs[i]);
+      newButtons.text(giffyGifs[i]);
+
+      $("#buttons").append(newButton);
+  }
+}
+renderButtons();
 
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dogs&api_key=uKbPHducH3lIMJpS3SQ3Qa3oqhcS4oSO&limit=20");
-xhr.done(function(data) { 
-    console.log("success got data", data); 
-    $(".images").append(JSON.stringify(data));
-});
+var button = $("<input>").attr({type: "button", name: "giffyButton", value:"Dogs"});
+    $("#buttons").append(button);
 
-/*for (var i = 0; i < data.length; i++) {
-    var gifImage = $("<img>");
-    gifImage.addClass("gif-image");
-    gifImage.attr("src", data[i]);
-    $(".images").append(gifImage);
-}*/
+
+var giffyGifs = ["dog", "cat", "canary", "goldfish", "hermit+crab", "finch", "bearded+dragon", "ferret"];
+// Function for dumping the JSON content for each entry into the div
+function displayGif() {
+
+    var gifImage = $(this).attr("giffyGif");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giffyGifs + "&api_key=uKbPHducH3lIMJpS3SQ3Qa3oqhcS4oSO&limit=20";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $(".images").innerHTML("<img>", "src="(JSON.stringify(response.preview_gif)));
+      renderButtons();
+    });
+  }
 
 });
