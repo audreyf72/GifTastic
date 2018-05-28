a4d0fbab810bb90167a6dc21dd529931c4692103
audrey <audreyfernandez@verizon.net>
2018-05-27 21:07:29 -0700

    Homework completed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# GifTastic
+# GIFtastic Animal Search
+
+API Homework Project #5 as part of the UCI Coding Bootcamp program. Use the Giphy API to make a dynamic web page that populates with gifs using javascript and jQuery to modify the HTML on the site.
+
+## How to Use
+
+Click on a button at the top of the page to view GIF images of the animal listed on that button. Use the text input feild on the right side of the page to create buttons and click on the new button to view gifs of the added animals. Click on the displayed images to animate them and click on them again to stop the animation.
+
+## Technologies used
+
+- Giphy API Database
+- HTML/CSS
+- Javascript
+- jQuery
+- Ajax
+- Visual Studio Code (Text editor)
--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,39 +1,80 @@
 $(document).ready(function() {
 
-//renders buttons
+// Initial array of animals
+var animals = ["dog", "cat", "bear", "lion"];
+
+// Function to dynamically generate buttons for each animal in the array
 function renderButtons() {
-  $('#buttons').empty();
-
-  for (var i = 0; i < giffyGifs.length; i++) {
-
-      var newButtons = $("<button class='btn btn-primary btn-lg active'>");
-      newButtons.attr("data-gif", giffyGifs[i]);
-      newButtons.text(giffyGifs[i]);
-
-      $("#buttons").append(newButton);
+$("#buttons-view").empty();
+  
+    for (var i = 0; i < animals.length; i++) {
+      var b = $("<button>");
+      b.addClass("animal");
+      b.attr("data-name", animals[i]);
+      b.text(animals[i]);
+      $("#buttons-view").append(b);
+    }
+    displayGifImages(); 
   }
-}
-renderButtons();
 
+// Function for retrieving and displaying API image content for each button clicked
+function displayGifImages() {
+$("button").on("click", function() {    
 
-var button = $("<input>").attr({type: "button", name: "giffyButton", value:"Dogs"});
-    $("#buttons").append(button);
+    var animal = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
+    animal + "&api_key=uKbPHducH3lIMJpS3SQ3Qa3oqhcS4oSO&limit=10&rating=pg";
 
-
-var giffyGifs = ["dog", "cat", "canary", "goldfish", "hermit+crab", "finch", "bearded+dragon", "ferret"];
-// Function for dumping the JSON content for each entry into the div
-function displayGif() {
-
-    var gifImage = $(this).attr("giffyGif");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giffyGifs + "&api_key=uKbPHducH3lIMJpS3SQ3Qa3oqhcS4oSO&limit=20";
-
-    $.ajax({
-      url: queryURL,
+  $.ajax({
+      url: queryURL, 
       method: "GET"
-    }).then(function(response) {
-      $(".images").innerHTML("<img>", "src="(JSON.stringify(response.preview_gif)));
-      renderButtons();
-    });
-  }
+    }).done(function(response) {
+  console.log(response);
 
+  $(".imageView").empty();
+    
+    for (i = 0; i < response.data.length; i++) {
+      var animalDisplay = $("<div class='animalDisplay'>");
+      var rating = response.data[i].rating;
+      var ratingText = $("<p id='rating'>").text("Rating: " + rating + " ");
+      var image = $("<img>");
+      var imageUrl = response.data[i].images.fixed_height.url;
+      var imageStillUrl = response.data[i].images.fixed_height_still.url;
+
+            image.addClass("gif");
+            image.attr("src", imageStillUrl);
+            image.attr("data-state", "still");
+            image.attr("data-still", imageStillUrl);
+            image.attr("data-animate", imageUrl);
+
+            $(image).on("click", function() {
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                 $(this).attr("src", $(this).attr("data-animate"));
+                 $(this).attr("data-state", "animate");
+                 } else {
+                 $(this).attr("src", $(this).attr("data-still"));
+                 $(this).attr("data-state", "still");
+                 }
+                 });
+
+            animalDisplay.append(ratingText);
+            animalDisplay.append(image);
+            $(".imageView").append(animalDisplay);
+      }
+  }); 
+}
+)}
+
+// Adds new buttons with animal names from input 
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    var animal = $("#gif-input").val().trim();
+    animals.push(animal);
+    renderButtons(); 
+    $("#gif-input").val("");      
+})
+
+// Function for displaying the initial animal data
+    renderButtons();
 });
